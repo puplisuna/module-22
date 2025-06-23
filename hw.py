79cd9dd063b99d440a5575ed82669382693601fd
puplisuna <ohombahttacharjee299@gmail.com>
2025-06-23 14:10:53 +0600

    h.w

---
+++ hw.py
@@ -0,0 +1,5 @@
+string1 = input("enter a string")
+string2 = input("enter another string")
+string3 = input("enter a third string")
+combined = string1 + string2 + string3 
+print("combined string: ", combined)
