b54479ea08e1cc3251027e138c1f093c3202b7d9
puplisuna <ohombahttacharjee299@gmail.com>
2025-06-27 10:40:30 +0600

    clas-2

---
+++ class-2/activity-2.py
@@ -0,0 +1,25 @@
+import turtle 
+
+turtle.Screen().bgcolor("orange")
+board = turtle.turtle()
+
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+
+board.pendown()
+board.right(90)
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+
+board.done()
