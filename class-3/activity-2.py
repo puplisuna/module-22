25a5b577c29ca1870937a59522930361acee2af9
puplisuna <ohombahttacharjee299@gmail.com>
2025-06-29 20:31:29 +0600

    ok

---
+++ class-3/activity-2.py
@@ -0,0 +1,21 @@
+list = ['apple', 'banana', 'cherry']
+print("leangth of list is: ", len(list))
+print("first element of list is: ", list[0])
+print("last element of list is: ", list[-1])
+print("sliced list is: ", list[1:3])
+print("reversed list is: ", list[::-1])
+
+list.append('mango')
+print("list after appending mango: ", list)
+list.remove('banana')
+print("list after removing banana: ", list)
+list.sort
+print("list after sorting: ", list)
+list.reverse()
+print("list after reversing: ", list)
+list.insert(1, 'orange')
+print("list after inserting orange at index 1: ", list)
+list.clear()
+print("list after clearing: ", list)
+
+
