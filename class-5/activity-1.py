7702226dbf5ea84739e929c10ba9da3c19669444
puplisuna <ohombahttacharjee299@gmail.com>
2025-07-06 20:19:44 +0600

    ok

---
+++ class-5/activity-1.py
@@ -0,0 +1,8 @@
+def absolute_value(num):
+    if num >= 0:
+        return num
+    else:
+        return -num
+    
+print("absolute value of 100 :", absolute_value(100))
+print("absolute value of -100 :", absolute_value(-100))
